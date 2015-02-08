1a312c82f7a60ceb2792b24db7976649adebf3df
stork <stork@not.gd>
2015-02-08 21:55:23 +0000

    tweaking stuff around

--- main.js
+++ main.js
@@ -5,32 +5,57 @@
 var config = require('config'),
     log = require('winston'),
     Rx = require('rx'),
+    argv = require('minimist')(process.argv.slice(2)),
     Scraper = require('./lib/scraper'),
     API = require('./lib/d3api'),
     Database = require('./lib/database');
 
-Rx.config.longStackSupport = true;
+Rx.config.longStackSupport = !!argv.v;
 
 log.cli();
-log.level = 'silly';
+log.level = argv.v ? 'silly' : 'info';
+
+var [region, clss, type, eraOrSeason] = argv._;
+
+if (!region || !clss) {
+    log.info("Missing arguments");
+    log.info("Required: <eu|na> <class> [era|season] [eraNum|seasonNum]");
+    log.info("Optional: -v (verbose), -h (hardcore)");
+    return;
+}
+
+type = type || 'era';
+eraOrSeason = eraOrSeason || 2;
 
 var scraper = new Scraper(config.get('endpoints.rankings')),
     api = new API(),
     db = new Database(config.get('database'));
 
-scraper.getHeroes('eu', 'barbarian', 'era', 2)
-    .take(20)
+var source = scraper.getHeroes(region, clss, type, eraOrSeason)
+    .take(100)
     .select((profile) => {
-        return api.getEligibleHeroes('eu', profile.name, profile.code, 'barbarian', false)
-            .select((heroStub) => api.getHeroData('eu', profile.name, profile.code, heroStub.id))
+        return api.getEligibleHeroes(region, profile.name, profile.code, clss, !!argv.h)
+            .merge(5)
+            .filter((heroStub) => !!heroStub)
+            .doOnNext((heroStub) => {
+                log.silly("Found eligible hero stub: ", heroStub);
+            })
+            .select((heroStub) => api.getHeroData(region, profile.name, profile.code, heroStub.id))
             .merge(1)
-            .selectMany((hero) => db.insertHero(hero, 'eu', profile.name, profile.code,
+            .selectMany((hero) => db.insertHero(hero, region, profile.name, profile.code,
                 profile.tier, profile.time))
-            .select(() => profile);
+            .count()
+            .filter((count) => count > 0)
+            .select((count) => [count, profile]);
     })
-    .merge(1)
-    .subscribe((hero) => {
-        console.dir(hero);
+    .merge(3)
+    .publish()
+    .refCount();
+
+source
+    .subscribe((result) => {
+        var [count, profile] = result;
+        log.info("Inserted %d records for:", count, profile);
     }, (err) => {
         console.dir(err);
         console.dir(err.stack);

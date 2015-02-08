c2fadec7a87d957d379e8e4d41c6926a9fd9d537
stork <stork@not.gd>
2015-02-08 11:28:12 +0000

    something is causing it to lock up and i dunno what it is

--- main.js
+++ main.js
@@ -3,5 +3,35 @@
  */
 
 var config = require('config'),
+    log = require('winston'),
+    Rx = require('rx'),
     Scraper = require('./lib/scraper'),
+    API = require('./lib/d3api'),
     Database = require('./lib/database');
+
+Rx.config.longStackSupport = true;
+
+log.cli();
+log.level = 'silly';
+
+var scraper = new Scraper(config.get('endpoints.rankings')),
+    api = new API(),
+    db = new Database(config.get('database'));
+
+scraper.getHeroes('eu', 'barbarian', 'era', 2)
+    .take(20)
+    .select((profile) => {
+        return api.getEligibleHeroes('eu', profile.name, profile.code, 'barbarian', false)
+            .select((heroStub) => api.getHeroData('eu', profile.name, profile.code, heroStub.id))
+            .merge(1)
+            .selectMany((hero) => db.insertHero(hero, 'eu', profile.name, profile.code,
+                profile.tier, profile.time))
+            .select(() => profile);
+    })
+    .merge(1)
+    .subscribe((hero) => {
+        console.dir(hero);
+    }, (err) => {
+        console.dir(err);
+        console.dir(err.stack);
+    });
---
+++ schema.sql
@@ -0,0 +1,76 @@
+CREATE TYPE item_slot AS ENUM (
+    'head', 'torso', 'feet', 'hands', 'shoulders', 'legs', 'bracers',
+    'mainHand', 'offHand', 'waist', 'rightFinger', 'leftFinger', 'neck'
+);
+
+CREATE TYPE skill_type AS ENUM (
+    'passive','skill', 'rune'
+);
+
+create table hero (
+    id SERIAL PRIMARY KEY,
+    hero_id integer not null UNIQUE,
+    name text not null,
+    class text not null,
+    battle_tag text not null,
+    ranking_tier integer not null,
+    ranking_time real not null,
+    last_updated integer not null,
+    paragon_level integer not null,
+    region text not null,
+    seasonCreated integer default null,
+    hardcore boolean not null,
+    last_modified timestamp not null default current_timestamp
+);
+
+create table hero_stats (
+    hero_id integer not null REFERENCES hero (id) ON DELETE CASCADE,
+    name text not null,
+    value real not null
+);
+
+create table items (
+    id SERIAL PRIMARY KEY,
+    hero_id integer not null REFERENCES hero (id) ON DELETE CASCADE,
+    item_id text not null,
+    name text not null,
+    icon text not null,
+    color text not null,
+    tooltip_params text not null,
+    slot item_slot not null,
+    data jsonb
+);
+
+create table item_attrs (
+    item_id integer not null REFERENCES items (id) ON DELETE CASCADE,
+    name text not null,
+    val_min real not null,
+    val_max real not null
+);
+
+create table item_gems (
+    item_id integer not null REFERENCES items (id) ON DELETE CASCADE,
+    gem_id text not null,
+    rank integer
+);
+
+create table hero_skills (
+    hero_id integer not null REFERENCES hero (id) ON DELETE CASCADE,
+    name text not null,
+    rune text,
+    type skill_type not null
+);
+
+create table skills (
+    id text not null,
+    parent_id text,
+    slug text not null,
+    name text not null,
+    level integer not null,
+    tooltip text not null,
+    description text not null,
+    class text not null,
+    icon text,
+    type  skill_type not null,
+    unique(id, parent_id, class, type)
+);

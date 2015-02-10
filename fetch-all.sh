9170412423c8342f3c6ec92e1e470731f787b2e2
stork <stork@not.gd>
2015-02-10 06:05:49 +0000

    yep.

--- fetch-all.sh
+++ fetch-all.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 NODE=nodejs
-CLASSES=(dh,wd,barbarian,crusader,monk,wizard)
 
-for class in "${classes[@]}"
-do
-    $NODE d3i2-bootstrap.js get na
+for clss in dh,wd,barbarian,crusader,monk,wizard; do
+    # $NODE d3i2-bootstrap.js get na $class
+    echo "$clss\n\n"
 done
